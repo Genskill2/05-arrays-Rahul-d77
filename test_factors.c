fa2363b2e9a832e1c81a86bf9f0021415b00954a
Rahul-d77 <84717074+Rahul-d77@users.noreply.github.com>
2021-06-19 08:59:16 +0530

    test_factors.c

--- test_factors.c
+++ test_factors.c
@@ -21,3 +21,50 @@ int main(void) {
   assert (ret[1] == 13);
   printf("Factors: passed\n");
 }
+
+
+
+
+int factors(int a, int c[])
+{ int k=0;
+  int temp=0;
+  int flag=0;
+  int b[50];
+  int fact=0;
+  for(int i=2;i<100;i++)
+  {
+    temp=0;
+    for(int j=2;j<=i/2;j++)
+    {
+      if(i%j==0)
+      {
+        temp=1;
+        break;
+      }    
+    } 
+    if (temp==0)
+     {
+        b[k]=i;
+        k++;
+      }
+    }
+     int div=a;
+     for(int n=0;n<a;n++)
+     {
+       for(int d=0;d<k;d++)
+        { 
+          if(div==1)
+          break;
+          else if(div%b[d]==0) 
+                 {
+                   flag++;
+                   c[fact]=b[d]; 
+                   fact++;
+                   div/=b[d];
+                   break;
+                 }
+       
+        }
+   }  
+  return  flag;
+}
