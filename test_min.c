225d56bda78704dfc468dccfd0965c9864ea0e9b
Rahul-d77 <84717074+Rahul-d77@users.noreply.github.com>
2021-06-19 11:02:49 +0530

    test_min.c

--- test_min.c
+++ test_min.c
@@ -12,24 +12,3 @@ int main(void) {
   assert (min(y, 1) == 5);
   printf("Min: passed\n");
   }
-
-
-
-
-
-
-
-
-
-int min(int a[],int b)
-{
-  int MIN=a[0];
-  for(int i=0;i<b;i++)
-  {
-    if (MIN>a[i]) 
-    {
-      MIN=a[i];    
-    }
-  }
-  return MIN;
-}
